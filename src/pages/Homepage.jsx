48f3776e53599273e1471a7c9396171011fc9011
Shaving Foam <paragvastrad37@gmail.com>
2025-06-23 17:35:32 +0530

    Backround and Toggle Button

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,18 @@
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Homepage = () =>{
+    return (
+        <div>
+            {/* Theme Toggle */}
+            <ThemeToggle />
+            {/* BackRound Effects */}
+            <StarBackground />
+            {/* NavBar */}
+
+            {/* Main Content */}
+
+            {/* Footer */}
+        </div>
+    );
+};
