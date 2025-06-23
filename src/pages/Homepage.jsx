8a9a04f73a418f6a810a6e79944486c81f0f4849
Shaving Foam <paragvastrad37@gmail.com>
2025-06-23 20:01:27 +0530

    aboutsectiondone

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,6 @@
+import { AboutSection } from "../components/AboutSection";
+import { HeroSection } from "../components/HeroSection";
+import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -9,8 +12,12 @@ export const Homepage = () =>{
             {/* BackRound Effects */}
             <StarBackground />
             {/* NavBar */}
-
+            <Navbar />
             {/* Main Content */}
+            <main>
+                <HeroSection />
+                <AboutSection />
+            </main>
 
             {/* Footer */}
         </div>
